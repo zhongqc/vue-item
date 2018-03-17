27260b92349034087e45a83b5d5c6aaefe583c39
Zhongqc <zhongqc7@gmail.com>
2018-03-17 17:25:13 +0800

    添加基础配置

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true
+  },
+  extends: [
+    'standard',
+    'plugin:vue/essential'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
