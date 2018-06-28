7b8c5ea0159374b03ea699f314cf89b2bc2cc38b
Zhongqc <zhongqc7@gmail.com>
2018-06-29 00:00:02 +0800

    modify webpack module config

--- build/webpack.module.conf.js
+++ build/webpack.module.conf.js
@@ -2,9 +2,21 @@
 const path = require('path')
 const webpackMerge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
+const isMinify = false
 
 const webpackConfig = webpackMerge(baseWebpackConfig, {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    path: path.join(__dirname, '../lib'),
+    library: 'vue-item',
+    libraryTarget: 'umd',
+    filename: isMinify ? '[name].min.js' : '[name].js',
+    umdNamedDefine: true,
+    globalObject: 'this'
+  },
+  optimization: {
+    minimize: isMinify
+  }
 })
 
 module.exports = webpackConfig
