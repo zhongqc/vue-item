569c123fb3d3df1fc47c1def73371d2aad1c7df7
Zhongqc <zhongqc7@gmail.com>
2018-07-29 19:19:19 +0800

    build add banner

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,9 +1,19 @@
+const version = require('../package.json').version
+
+const banner =
+`/*!
+  * VueItem v${version}
+  * (c) 2018-${new Date().getFullYear()} Zhongqc
+  * Released under the MIT License.
+  */`
+
 export default {
   input: 'esModule/index.js',
   output: {
     file: 'lib/vue-item.js',
     format: 'umd',
     name: 'VueItem',
-    sourceMap: true
+    sourcemap: true,
+    banner
   }
 }
