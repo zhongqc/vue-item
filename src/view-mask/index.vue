27824314e4654b00c23a8c6521484ae3d2713b0c
Zhongqc <zhongqc7@gmail.com>
2018-08-23 23:39:50 +0800

    add view-mask commponent

---
+++ src/view-mask/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="item-view-mask" @click="$emit('viewMaskClick')" v-show="showFlag"></div>
+</template>
+<script>
+export default {
+  name: 'view-mask',
+  props: {
+    showFlag: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
