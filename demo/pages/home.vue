3efef26626d5ad3d8ba9d3f7369596e496d87df1
Zhongqc <zhongqc7@gmail.com>
2018-08-30 00:18:27 +0800

    add demo home.vue

---
+++ demo/pages/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <item-nav-bar title="Vue Item" leftText="back" :backArrow="true" rightText="right"></item-nav-bar>
+    <item-cell-group v-for="list of ComponentList" :key="list.title" :groupTitle="list.title">
+      <item-cell v-for="component of list.list" :key="component.title" @cellClick="$router.push(component.link)">
+        {{ component.title }}
+      </item-cell>
+    </item-cell-group>
+  </div>
+</template>
+<script>
+import ComponentList from '../assets/json/component-list'
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Hello Vue-Item',
+      subMsg: 'Hello test',
+      ComponentList
+    }
+  },
+  methods: {}
+}
+</script>
