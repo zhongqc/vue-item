a55a812102eb72da09bd25b2efb237023f8776bf
Zhongqc <zhongqc7@gmail.com>
2018-09-16 16:45:16 +0800

    dialog basic demo

--- demo/pages/dialog.vue
+++ demo/pages/dialog.vue
@@ -1,5 +1,8 @@
 <template>
-  <item-dialog title="提示" :buttons="alertBtn">消息</item-dialog>
+  <div class="dialog-demo">
+    <item-nav-bar title="Dialog" leftText="返回" :backArrow="true" @leftClick="$router.back()" />
+    <item-dialog title="提示" :buttons="alertBtn" :show="dialogShow" @dialogViewMaskClick="dialogViewMaskClick">消息</item-dialog>
+  </div>
 </template>
 <script>
 export default {
@@ -9,8 +12,16 @@ export default {
       alertBtn: [{
         text: '确认',
         className: '',
-        click: () => { console.log(1) }
-      }]
+        click: () => {
+          this.dialogShow = false
+        }
+      }],
+      dialogShow: true
+    }
+  },
+  methods: {
+    dialogViewMaskClick (e) {
+      this.dialogShow = false
     }
   }
 }
